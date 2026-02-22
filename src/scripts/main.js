af0e5ce51d89140b9a27623141466de11f1ca9c6
Kristina Kovalchuk <kristina36247@gmail.com>
2026-02-22 16:30:53 +0300

    finish header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,48 @@
 'use strict';
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  const slides = document.querySelectorAll(
+    ".header__bottom__switch-progress-1, .header__bottom__switch-progress-2, .header__bottom__switch-progress-3"
+  );
+
+  const btnLeft = document.querySelector(".header__bottom__switch-btn-left");
+  const btnRight = document.querySelector(".header__bottom__switch-btn-right");
+
+  let currentIndex = 0;
+
+  const totalSlides = slides.length;
+
+  function showSlide(index) {
+
+    slides.forEach((slide) => {
+
+      slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+
+    btnLeft.disabled = index === 0;
+    btnRight.disabled = index === totalSlides - 1;
+  }
+
+  btnRight.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (currentIndex < totalSlides - 1) {
+      currentIndex++;
+      showSlide(currentIndex);
+    }
+  });
+
+  btnLeft.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (currentIndex > 0) {
+      currentIndex--;
+      showSlide(currentIndex);
+    }
+  });
+
+  showSlide(currentIndex);
+});
